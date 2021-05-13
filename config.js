c91ee05d3a61beaffb7f819e3b6bb3a77f8e30f8
abdulganiyy <lasisibalogunopeoluwa@ymail.com>
2021-05-13 23:16:22 +0100

    files edited

---
+++ config.js
@@ -0,0 +1,5 @@
+module.exports = {
+  MONGODB:
+    "mongodb+srv://abdulganiyy:Ope12oluwa@@cluster0.huyey.mongodb.net/graphql?retryWrites=true&w=majority",
+  SECRET_KEY: "some secret key here",
+};
